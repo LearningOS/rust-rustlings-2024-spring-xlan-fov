39dba673102d75c86d1f8522bc7084e74452e364
xlan-fov <83161926+xlan-fov@users.noreply.github.com>
2024-04-21 08:22:20 +0000

    update

--- exercises/error_handling/errors5.rs
+++ exercises/error_handling/errors5.rs
@@ -29,8 +29,8 @@ use std::fmt;
 use std::num::ParseIntError;
 
 // TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), Box<dyn ???>> {
-    let pretend_user_input = "42";
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let pretend_user_input ="42";
     let x: i64 = pretend_user_input.parse()?;
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
     Ok(())
--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -25,13 +25,18 @@ impl ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
     // TODO: add another error conversion function here.
-    // fn from_parseint...
+ fn from_parseint (err:ParseIntError)->ParsePosNonzeroError{
+    ParsePosNonzeroError::ParseInt(err)
+ }
+ fn handler(e:ParseIntError)->ParsePosNonzeroError{
+    ParsePosNonzeroError::from_parseint(e)
+ }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;//unwrap();
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
