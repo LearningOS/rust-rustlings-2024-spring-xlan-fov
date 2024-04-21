6d4b606d6c17b6295e7009ded5e7dce88b3850dc
xlan-fov <83161926+xlan-fov@users.noreply.github.com>
2024-04-21 08:11:39 +0000

    update

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,8 +3,7 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+#[derive(Clone)]
 struct Point {
     x: i32,
     y: i32,
@@ -13,7 +12,7 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    match y.clone() {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
